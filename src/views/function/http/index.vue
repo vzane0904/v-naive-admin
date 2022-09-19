48e6641a7763ac31d97187fa442014994f49ccf2
yc6 <17737266888@163.com>
2022-09-19 13:24:07 +0800

    feat(components): http / sessionTimeout Page

--- src/views/function/http/index.vue
+++ src/views/function/http/index.vue
@@ -1,9 +1,26 @@
 <template>
-  <Content> <NButton @click="errClick401">get 401 </NButton></Content>
+  <Content>
+    <NButton
+      type="primary"
+      v-for="item of list"
+      :key="item.name"
+      @click="item.fn()"
+      class="mr-10px"
+      >{{ item.name }}</NButton
+    >
+  </Content>
 </template>
 <script lang="ts" setup>
-import { get401 } from '@/api/test'
-
-const errClick401 = () => get401()
+import { getErr } from '@/api/test'
+const list = [
+  { name: '200', fn: () => getErr(200) },
+  { name: '206', fn: () => getErr(206) },
+  { name: '402', fn: () => getErr(402) },
+  { name: '403', fn: () => getErr(403) },
+  { name: '404', fn: () => getErr(404) },
+  { name: '500', fn: () => getErr(500) },
+  { name: '501', fn: () => getErr(501) },
+  { name: '503', fn: () => getErr(503) },
+]
 </script>
 <style scoped></style>
